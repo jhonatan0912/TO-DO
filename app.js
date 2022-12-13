372b35df3f8f4995fcee082043eb4cb867cfcc29
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-12-13 13:45:18 -0500

    shesh

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
-let form = document.getElementById("form");
-
-let itemsContainer = document.getElementById("items-container");
-
-
+const form = document.getElementById('form');
+let input = document.getElementById('input');
+let itemsContainer = document.getElementById('items-container');
 let count = 0;
-let countTemplate = `<div><div>${count + 1}</div></div>`
 
 form.addEventListener("submit", e => {
   e.preventDefault();
-  let itemValue = document.getElementById("input").value;
-  let item = document.createElement("div");
-  item.innerHTML = `
-  <div class="flex justify-between items-center border-b p-3">
-    <div>${itemValue}</div>
-    <div>
-      <img src="./images/icon-cross.svg" class="btn-delete cursor-pointer">
-    </div>
-  </div>
-  ${countTemplate}
-  `;
-  itemsContainer.append(item)
+  let itemBox = document.createElement('div');
+  itemBox.innerHTML = `
+  <div class="border-b flex justify-between p-3 items-center" id="item${count += 1}">
+    <div>${input.value}</div>
+    <img src="./images/icon-cross.svg" class="cursor-pointer btn-delete">
+    </div>`;
+  itemsContainer.append(itemBox);
 })
-
-
-let btnDelete = document.querySelector(".btn-delete");
-
-btnDelete.addEventListener("click", e => {
-  console.log(e.target.parentElement.parentElement.remove());
-})
-
-
-
